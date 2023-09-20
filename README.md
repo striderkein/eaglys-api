f201033057384e1409a2abe77f922fdd25f63c6c
shirow ozawa <striderkein@users.noreply.github.com>
2023-09-20 21:51:02 +0900

    feat:impl feature-2

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ npm start
 ## endpoints
 
 * `/parse-sql`
+* `/modify-ast`
 
 ### `/parse-sql`
 
@@ -85,4 +86,28 @@ curl -X POST -H 'Content-Type: application/json' -d '{"sql":"SELECT * FROM USERS
         "window": null
     }
 }
+```
+
+### `/modify-ast`
+
+request
+
+```shell
+curl -X POST -H 'Content-Type: application/json' -d '{"sql":"SELECT NAME FROM USERS WHERE USER_ID = 1 AND AGE = 10"}' http://localhost:3000/modify-ast
+```
+
+response
+
+```json
+[
+  {
+    "NAME": "eaa58932"
+  },
+  {
+    "USER_ID": "e5bb97d1"
+  },
+  {
+    "AGE": "ab864bbd"
+  }
+]
 ```
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const { Parser } = require('node-sql-parser');
 
+const { hashColumnNames } = require('./util');
+
 const app = express();
 const port = 3000;
 
@@ -22,6 +24,31 @@ app.post('/parse-sql', (req, res) => {
   const sqlQuery = req.body.sql;
   const ast = parser.astify(sqlQuery);
   res.status(201).json({ ast });
+});
+
+// APIエンドポイント2: カラム名をハッシュ化し、マップを保存
+app.post('/modify-ast', (req, res) => {
+  const parser = new Parser();
+  const sqlQuery = req.body.sql;
+  const ast = parser.astify(sqlQuery);
+
+  // カラム名のハッシュ化とマップの作成
+  const columnMap = hashColumnNames(ast);
+
+  console.log(`columnMap: ${JSON.stringify(columnMap)}`)
+  // SQLiteデータベースにマップを保存
+  db.serialize(() => {
+    const stmt = db.prepare("INSERT INTO column_map VALUES (?, ?)");
+
+    for (const entry of columnMap) {
+      const original = Object.keys(entry)[0];
+      const hashed = entry[original];
+      stmt.run(original, hashed);
+    }
+
+    stmt.finalize();
+    res.json(columnMap);
+  });
 });
 
 // サーバーの起動
